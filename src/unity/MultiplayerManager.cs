481e1dc8348de6d493e1be7638036efd0c82475e
Sbauermaner <34747760+Sbauermaner@users.noreply.github.com>
2025-02-06 17:39:26 +0100

    Create MultiplayerManager.cs

---
+++ src/unity/MultiplayerManager.cs
@@ -0,0 +1,14 @@
+using Photon.Pun;
+
+public class MultiplayerManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+}
