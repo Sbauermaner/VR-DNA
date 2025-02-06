7426cee2cf03904f50aa76c37e9099c99010e690
Sbauermaner <34747760+Sbauermaner@users.noreply.github.com>
2025-02-06 16:35:16 +0100

    Update DNAGrabbable.cs

--- src/unity/DNAGrabbable.cs
+++ src/unity/DNAGrabbable.cs
@@ -1,46 +1,11 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-using System.Collections;
-using UnityEngine.Networking;
-using System.Text;
 
 public class DNAGrabbable : XRGrabInteractable
 {
-    public Transform leftGrab, rightGrab;
-    public LineRenderer dnaStrand;
-    private float originalLength;
-
-    void Start()
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
-        originalLength = Vector3.Distance(leftGrab.position, rightGrab.position);
-    }
-
-    void Update()
-    {
-        float currentLength = Vector3.Distance(leftGrab.position, rightGrab.position);
-        float scaleMultiplier = Mathf.Clamp(currentLength / originalLength, 0.8f, 1.5f);
-        dnaStrand.transform.localScale = new Vector3(1, 1, scaleMultiplier);
-
-        if (scaleMultiplier > 1.2f)
-        {
-            GetComponent<Renderer>().material.color = Color.red;
-            StartCoroutine(AnalyzeStretch(scaleMultiplier));
-        }
-        else
-        {
-            GetComponent<Renderer>().material.color = Color.white;
-        }
-    }
-
-    IEnumerator AnalyzeStretch(float stretchFactor)
-    {
-        string json = $"{{\"stretch_values\": [{stretchFactor}]}}";
-        using (UnityWebRequest request = UnityWebRequest.Post("http://localhost:5000/analyze_dna", json))
-        {
-            byte[] body = Encoding.UTF8.GetBytes(json);
-            request.uploadHandler = new UploadHandlerRaw(body);
-            yield return request.SendWebRequest();
-            Debug.Log("Mutation risk: " + request.downloadHandler.text);
-        }
+        base.OnSelectEntered(args);
+        Debug.Log("DNA strand selected.");
     }
 }
