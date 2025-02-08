09277ad3e1fa2fa5d9dcc9c4cb58d63398cbf586
Sbauermaner <34747760+Sbauermaner@users.noreply.github.com>
2025-02-08 10:55:55 +0100

    Update dna_3d.py

--- src/python/dna_3d.py
+++ src/python/dna_3d.py
@@ -1,12 +1,19 @@
-import open3d as o3d
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-def generate_dna():
-    theta = np.linspace(0, 4*np.pi, 100)
-    x = np.sin(theta)
-    y = np.cos(theta)
-    z = np.linspace(0, 1, 100)
-    points = np.column_stack([x, y, z])
-    cloud = o3d.geometry.PointCloud()
-    cloud.points = o3d.utility.Vector3dVector(points)
-    o3d.visualization.draw_geometries([cloud])
+def dna_helix(n_turns=10, points_per_turn=50):
+    """Генерация спирали ДНК в 3D"""
+    theta = np.linspace(0, 2 * np.pi * n_turns, points_per_turn * n_turns)
+    x = np.cos(theta)
+    y = np.sin(theta)
+    z = np.linspace(0, n_turns, points_per_turn * n_turns)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(x, y, z, label="ДНК спираль")
+    ax.legend()
+    plt.show()
+
+# Пример
+dna_helix()
